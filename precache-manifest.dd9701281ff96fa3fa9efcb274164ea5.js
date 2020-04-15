fad44718cb55fcb681e6309e6658623a51fd0e22
Erez Nagar <erez.nagar@gmail.com>
2020-04-15 01:16:23 -0400

    Updates

---
+++ precache-manifest.dd9701281ff96fa3fa9efcb274164ea5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d48f78d6b2020428e8c8ed38ac5429",
+    "url": "/lease-calculator-app/index.html"
+  },
+  {
+    "revision": "7aeaca3cba746c80e15d",
+    "url": "/lease-calculator-app/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "bb01af86eb8ec905a6a4",
+    "url": "/lease-calculator-app/static/css/main.816fc9ce.chunk.css"
+  },
+  {
+    "revision": "7aeaca3cba746c80e15d",
+    "url": "/lease-calculator-app/static/js/2.1ecf7d7d.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/lease-calculator-app/static/js/2.1ecf7d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb01af86eb8ec905a6a4",
+    "url": "/lease-calculator-app/static/js/main.95113609.chunk.js"
+  },
+  {
+    "revision": "6aa36dd2fbe6ae31a020",
+    "url": "/lease-calculator-app/static/js/runtime-main.3a0eb232.js"
+  }
+]);
